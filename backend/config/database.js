7b1ba2281c6ca9c50871b01cfbc04b377eaa09bb
Kartik <freedomyeager12@gmail.com>
2024-02-26 23:30:43 -0500

    Added database.js env.js logger.js in the config folder

---
+++ backend/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { MONGODB_URI } = process.env;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // any other options
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
